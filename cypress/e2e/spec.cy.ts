b1a4e00dac19070f45c890ca025a6aa7bd7cca06
Steven Kitterman <steven.kitterman@gmail.com>
2023-12-08 09:25:02 -0800

    Included test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,21 +6,21 @@ describe('template spec', () => {
     cy.title().should('equal', 'Mealdrop - find your next meal')
   })
 
-  // it('creates a new order', () => {
-  //   cy.visit('http://localhost:3000/restaurants/2')
+  it('creates a new order', () => {
+    cy.visit('http://localhost:3000/restaurants/2')
 
-  //   cy.wait(1000)
-  //   // @ts-expect-error
-  //   cy.takeChromaticArchive()
+    cy.wait(1000)
+    // @ts-expect-error
+    cy.takeChromaticArchive()
 
-  //   cy.contains('h4', 'Fries').click()
+    cy.contains('h4', 'Fries').click()
 
-  //   cy.get('[data-testid="modal"]').should('be.visible')
-  //   // @ts-expect-error
-  //   cy.takeChromaticArchive()
+    cy.get('[data-testid="modal"]').should('be.visible')
+    // @ts-expect-error
+    cy.takeChromaticArchive()
 
-  //   cy.contains('button', /add for €2\.50/).click()
+    cy.contains('button', /add for €2\.50/).click()
 
-  //   cy.contains('[data-testid="header"] span', '€2.50').should('be.visible')
-  // })
+    cy.contains('[data-testid="header"] span', '€2.50').should('be.visible')
+  })
 })
