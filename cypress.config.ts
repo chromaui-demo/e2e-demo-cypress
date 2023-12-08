5e3e25ec7feadd2f683e547e48d2e2c90e3d018e
Steven Kitterman <steven.kitterman@gmail.com>
2023-12-07 16:49:41 -0800

    Chromatic e2e added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'cypress'
+import { archiveCypress } from '@chromaui/test-archiver/cypress'
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        archiveCypress,
+      })
     },
   },
 })
