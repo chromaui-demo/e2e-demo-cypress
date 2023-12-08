d65629f656616595cd9fc6af9d0cf29f1d767ea8
Steven Kitterman <steven.kitterman@gmail.com>
2023-12-08 08:36:28 -0800

    Second test added

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,24 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('loads homepage', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.title().should('equal', 'Mealdrop - find your next meal')
+  })
+
+  it('creates an order', () => {
+    cy.visit('http://localhost:3000/restaurants/2')
+
+    cy.wait(1000)
+    // await takeArchive(page, testInfo)
+
+    cy.contains('h4', 'Fries').click()
+
+    cy.get('[data-testid="modal"]').should('be.visible')
+
+    // await takeArchive(page, testInfo)
+
+    cy.contains('button', /add for €2\.50/).click()
+
+    cy.contains('[data-testid="header"] span', '€2.50').should('be.visible')
   })
 })
